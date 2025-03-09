f9229d5a95a0da5d694b8398e7330711ef8531a3
raisyramos <raisymaeramos20@gmail.com>
2025-03-09 09:06:04 +0800

    Number Checker
    Created a function which use the number as an input and check it if is an odd or even number.

---
+++ numberchecker.py
@@ -0,0 +1,11 @@
+def check_number(num):
+    if num % 2 == 0:
+        print(f"{num} is an even number.")
+    else:
+        print(f"{num} is an odd number.")
+
+check_number(49)
+check_number(22)
+check_number(0)
+        
+        
